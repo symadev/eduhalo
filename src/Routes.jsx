a81cc2a78410b9f4fb8ed55958c921178b043313
syma sultana <symasultana2002@gmail.com>
2025-07-11 10:45:34 +0600

    added the files

---
+++ src/Routes.jsx
@@ -0,0 +1,25 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./Components/MainLayout";
+import Home from "./Components/Home";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+
+      }
+    ]
+  },
+]);
+
+
+
+export default router;
